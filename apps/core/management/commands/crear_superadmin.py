5c59ae454ee58db63ae0c7a2e541f104e9827ba6
Fermat <jorge.ronconi@gmail.com>
2025-09-30 21:34:19 -0300

    comando perzonalizado para creacion del perfil admin

---
+++ apps/core/management/commands/crear_superadmin.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from apps.usuarios.models import UserProfile
+
+
+class Command(BaseCommand):
+    help = 'Crea un superusuario con perfil de superadmin'
+
+    def handle(self, *args, **options):
+        username = input('Username: ')
+        email = input('Email: ')
+        password = input('Password: ')
+
+        user = User.objects.create_superuser(username, email, password)
+        profile, created = UserProfile.objects.get_or_create(user=user)
+        profile.es_superadmin = True
+        profile.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(f'Superadmin "{username}" creado exitosamente')
+        )
